b87ae5803a4d236334353af7dda05d0c6a8b92d7
mrloczek <lokigra@gmail.com>
2020-05-30 16:11:16 +0200

    Project setup, dependencies, scaffholding

---
+++ src/app/router.ts
@@ -0,0 +1,6 @@
+import * as express from "express";
+
+export const createRouter = () => {
+    const router = express.Router();
+    router.use("/auth")
+}
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import * as http from "http"
+import { app } from "./app/app"
+
+(async ()=>{
+    const port = process.env.APP_PORT || 3000
+    const server = http.createServer(app)
+    server.listen(port)
+})()
